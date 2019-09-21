66c31d17fa44f6d3c7693d2f7245a44d5ade1b54
Robert Shamus <bshamus630@gmail.com>
2019-09-21 13:17:21 -0400

    Main Camera Added and functional

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Camera;
+    public Vector3 centerPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        centerPoint = new Vector3(0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        centerPoint = Camera.transform.position;
+        if (Player.transform.position.x > (centerPoint.x + 5.4f))
+        { this.gameObject.transform.position += new Vector3(10.8f, 0f, 0f);}
+        else if (Player.transform.position.y > (centerPoint.y + 4.05f))
+        { this.gameObject.transform.position += new Vector3(0f, 8.1f, 0f);}
+        else if (Player.transform.position.x < (centerPoint.x - 5.4f))
+        { this.gameObject.transform.position += new Vector3(-10.8f, 0f, 0f);}
+        else if (Player.transform.position.y < (centerPoint.y - 4.05f))
+        { this.gameObject.transform.position += new Vector3(0f, -8.1f, 0f);}
+        else {};
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() //Raycast collision fixes needed
+    {
+        if(Input.GetKey(KeyCode.A))
+        {this.gameObject.transform.position += new Vector3(-speed, 0, 0);}
+        if (Input.GetKey(KeyCode.W))
+        { this.gameObject.transform.position += new Vector3(0, speed, 0); }
+        if (Input.GetKey(KeyCode.S))
+        { this.gameObject.transform.position += new Vector3(0, -speed, 0); }
+        if (Input.GetKey(KeyCode.D))
+        { this.gameObject.transform.position += new Vector3(speed, 0, 0); }
+
+    }
+}
